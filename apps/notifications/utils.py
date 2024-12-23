55787079cadee252b80912834b3b8728b3ba3679
skalyar27 <skalyar27@gmail.com>
2024-12-23 17:52:07 +0300

    changed send notification logic

--- apps/notifications/utils.py
+++ apps/notifications/utils.py
@@ -14,28 +14,30 @@ def send_notifications(company_id: int, quiz_title: str, quiz_company_name: str)
     members = CompanyMember.objects.filter(company_id=company_id).select_related('user').only('user')
     channel_layer = get_channel_layer()
 
+    notifications_to_save = []
+
     for member in members:
         try:
             group_name = f"notifications_{member.user.id}"
-            
+
             notification = Notification(
                 user=member.user,
                 text=f"New quiz '{quiz_title}' now available in company {quiz_company_name}!"
             )
-            
-            notification_data = NotificationSerializer(notification).data
 
+            notification_data = NotificationSerializer(notification).data
             notification_message = {
                 "type": "new_notification",
                 "notification": notification_data,
             }
 
             async_to_sync(channel_layer.group_send)(
-                group_name, notification_message
+                group_name, notification_message    
             )
-            
-            notification.save()
-            
+
+            notifications_to_save.append(notification)
+
         except Exception as e:
             logger.error(f"Error sending notification for user {member.user}: {e}")
-    
+
+    Notification.objects.bulk_create(notifications_to_save)
